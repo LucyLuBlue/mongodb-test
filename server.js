b15cbb1ebb78f4256ff4c5c43933cddedb05372f
Lucy Lagunas <lucylu858@gmail.com>
2020-03-05 14:06:26 +0000

    2commint

--- server.js
+++ server.js
@@ -16,3 +16,11 @@ app.use(require('./routes'));
 app.listen(port,function(err){
     console.log("listening on Port: " + port)
 });
+mongoose.connect('mongodb://localhost/test');
+mongoose.connection.on('error', (err) => { 
+    console.log('Mongodb Error: ', err); 
+    process.exit();
+});
+mongoose.connection.on('connected', () => { 
+    console.log('MongoDB is successfully connected');
+});
